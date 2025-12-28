658974bc48bf3fcc67fed7fbfc48f7b438d0cbf5
inczert <robert.incze0@gmail.com>
2025-12-28 17:57:47 +0100

    fix(HardRT): Document test results on timing.

--- examples/hardrt_h755_blinky/src/main.c
+++ examples/hardrt_h755_blinky/src/main.c
@@ -1,72 +1,91 @@
-#include <stdbool.h>
 #include <stdint.h>
+#include "stm32h7xx.h"
+
 #include "hardrt.h"
 #include "hardrt_time.h"
-#include "stm32h7xx.h"
-#include "stm32h7xx_hal.h"
 
+/* NUCLEO-H755ZI-Q LEDs:
+ * LD1 (green)  = PB0
+ * LD2 (yellow) = PE1
+ * LD3 (red)    = PB14
+ */
+#define LED1_GPIO        GPIOB
+#define LED1_ENR         RCC_AHB4ENR_GPIOBEN
+#define LED1_PIN         0u    /* PB0 */
 
-#define HRT_STACK_SIZE 1024
+#define LED2_GPIO        GPIOE
+#define LED2_ENR         RCC_AHB4ENR_GPIOEEN
+#define LED2_PIN         1u    /* PE1 */
 
-extern void SystemInit(void);
-extern uint32_t SystemCoreClock;
+static inline void hold_cm4(void)
+{
+#define RCC_BASE_NEW   0x58024400UL
+#define RCC_GCR        (*(volatile uint32_t*)(RCC_BASE_NEW + 0x0))
+#define RCC_GRSTCSETR  (*(volatile uint32_t*)(RCC_BASE_NEW + 0x8))
+    RCC_GCR &= ~(1u << 0);
+    RCC_GRSTCSETR = (1u << 0);
+}
 
-static uint32_t stackA[1024], stackB[1024];
+static void gpio_init(void)
+{
+    RCC->AHB4ENR |= (LED1_ENR | LED2_ENR);
+    __asm volatile ("dsb sy");
 
-volatile uint32_t dbg_counterA = 0;
-volatile uint32_t dbg_counterB = 0;
-volatile uint32_t dbg_exit_counterA = 0;
-volatile uint32_t dbg_exit_counterB = 0;
+    /* PB0 output */
+    LED1_GPIO->MODER &= ~(3u << (LED1_PIN * 2));
+    LED1_GPIO->MODER |=  (1u << (LED1_PIN * 2));
+    LED1_GPIO->OTYPER &= ~(1u << LED1_PIN);
 
-static void TaskA(void* arg) {
+    /* PE1 output */
+    LED2_GPIO->MODER &= ~(3u << (LED2_PIN * 2));
+    LED2_GPIO->MODER |=  (1u << (LED2_PIN * 2));
+    LED2_GPIO->OTYPER &= ~(1u << LED2_PIN);
+}
+
+#define STACK_WORDS 512
+static uint32_t stackA[STACK_WORDS] __attribute__((aligned(8)));
+static uint32_t stackB[STACK_WORDS] __attribute__((aligned(8)));
+
+static void TaskA(void *arg)
+{
     (void)arg;
-    for(;;) {
-        dbg_counterA++;
-        (void)dbg_counterA;
-        hrt_sleep(5000);
-        dbg_exit_counterA++;
-        (void)dbg_exit_counterA;
+    for (;;) {
+        LED1_GPIO->ODR ^= (1u << LED1_PIN);  /* LD1 */
+        hrt_sleep(250);
     }
 }
-static void TaskB(void* arg) {
+
+static void TaskB(void *arg)
+{
     (void)arg;
-    for(;;) {
-        dbg_counterB++;
-        (void)dbg_counterB;
-        hrt_sleep(10000);
-        dbg_exit_counterB++;
-        (void)dbg_exit_counterB;
+    for (;;) {
+        LED2_GPIO->ODR ^= (1u << LED2_PIN);  /* LD2 */
+        hrt_sleep(500);
     }
 }
 
-
-static inline void hold_cm4(void){
-#define RCC_BASE_NEW 0x58024400UL
-#define RCC_GCR (*(volatile uint32_t*)(RCC_BASE_NEW + 0x0))
-#define RCC_GRSTCSETR (*(volatile uint32_t*)(RCC_BASE_NEW + 0x8))
-    RCC_GCR &= ~(1u<<0);         // don't boot CM4
-    RCC_GRSTCSETR = (1u<<0);     // assert CM4 reset
-}
-
-int main(void){
+int main(void)
+{
     SystemInit();
     hold_cm4();
+    gpio_init();
 
     hrt_config_t cfg = {
-        .tick_hz = 1000,
-        .policy  = HRT_SCHED_PRIORITY_RR,
-        .default_slice = 5,
-        .core_hz = SystemCoreClock,
-        .tick_src = HRT_TICK_SYSTICK
+        .tick_hz        = 1000,
+        .policy         = HRT_SCHED_PRIORITY_RR,
+        .default_slice  = 5,
+        .core_hz        = SystemCoreClock,
+        .tick_src       = HRT_TICK_SYSTICK
     };
+
     hrt_init(&cfg);
 
-    hrt_task_attr_t a0 = { .priority=HRT_PRIO0, .timeslice=5 };
-    hrt_task_attr_t a1 = { .priority=HRT_PRIO1, .timeslice=5 };
-    hrt_create_task(TaskA, 0, stackA, sizeof(stackA)/sizeof(stackA[0]), &a0);
-    hrt_create_task(TaskB, 0, stackB, sizeof(stackB)/sizeof(stackB[0]), &a1);
+    hrt_task_attr_t tA = { .priority = HRT_PRIO0, .timeslice = 5 };
+    hrt_task_attr_t tB = { .priority = HRT_PRIO1, .timeslice = 5 };
+
+    hrt_create_task(TaskA, 0, stackA, STACK_WORDS, &tA);
+    hrt_create_task(TaskB, 0, stackB, STACK_WORDS, &tB);
 
     hrt_start();
-    //for(;;) __asm volatile("wfi");
-    return 1;
+    return 0;
 }
