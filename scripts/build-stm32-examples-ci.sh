7396141237454334ac6c3461e39b10a48e4a83f5
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-09-03 19:19:20 +0200

    test(hardrt): cross-build scheduler timing diagnostic

--- scripts/build-stm32-examples-ci.sh
+++ scripts/build-stm32-examples-ci.sh
@@ -78,6 +78,7 @@ build_app hardrt_h755_ipc_queue "$ROOT_DIR/examples/hardrt_h755_ipc_validation" 
 build_app hardrt_h755_ipc_mutex "$ROOT_DIR/examples/hardrt_h755_ipc_validation" "$BASE_INSTALL" -DHARDRT_IPC_CASE=mutex
 build_app hardrt_h755_external_tick "$ROOT_DIR/examples/hardrt_h755_external_tick" "$BASE_INSTALL"
 build_app hardrt_h755_dwt_event_to_task "$ROOT_DIR/examples/hardrt_h755_dwt_timing" "$BASE_INSTALL" -DHARDRT_TIMING_CASE=event_to_task -DHARDRT_TIMING_TARGET_SAMPLES=8
+build_app hardrt_h755_dwt_scheduler_decision "$ROOT_DIR/examples/hardrt_h755_dwt_timing" "$BASE_INSTALL" -DHARDRT_TIMING_CASE=scheduler_decision -DHARDRT_TIMING_TARGET_SAMPLES=8
 
 IPC_BUILD="$ROOT_DIR/build-cortex_m-timing-ipc-ci"; IPC_INSTALL="$ROOT_DIR/install-cortexm-timing-ipc-ci"; IPC_HOOK_HEADER="$ROOT_DIR/examples/hardrt_h755_dwt_timing/inc/hardrt_timing_hooks.h"
 configure_timing_library "$IPC_BUILD" "$IPC_INSTALL" "$IPC_HOOK_HEADER"
