4ca65a014659f6821482edb7601260446d022977
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-09-05 03:00:20 +0200

    test(hardrt): compile C++ wrapper aliases

--- tests/cpp_contracts/queue_ok.cpp
+++ tests/cpp_contracts/queue_ok.cpp
@@ -1,17 +1,27 @@
 #include "hardrtpp.hpp"
 
 #include <cstdint>
+#include <type_traits>
 
 struct Packet {
     std::uint32_t id;
     std::uint16_t flags;
 };
 
-static_assert(std::is_trivially_copyable<Packet>::value, "test payload must be trivially copyable");
+static_assert(std::is_trivially_copyable<Packet>::value,
+              "test payload must be trivially copyable");
+static_assert(std::is_same<hardrt::Queue<Packet, 4>,
+                           hardrt::StaticQueue<Packet, 4>>::value,
+              "Queue<T,N> must remain an alias for StaticQueue<T,N>");
 
 hardrt::QueueRef<Packet> g_ref;
 hardrt::StaticQueue<Packet, 4> g_static;
+hardrt::Queue<Packet, 4> g_alias;
 
 int main() {
+    const char *canonical = hardrt::System::version_string();
+    const char *alias = hardrt::System::version();
+    (void)canonical;
+    (void)alias;
     return 0;
 }
