93785665ddf7026823df719fb07542031fb256f7
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-09-05 20:05:59 +0200

    docs(hardrt): document public tick source type

--- inc/hardrt.h
+++ inc/hardrt.h
@@ -134,6 +134,7 @@ typedef enum {
     HRT_PRIO11
 } hrt_prio_t;
 
+/** Tick-source ownership mode selected at initialization. */
 typedef enum {
     HRT_TICK_SYSTICK = 0,
     HRT_TICK_EXTERNAL = 1
@@ -172,7 +173,7 @@ typedef struct {
     uint16_t timeslice;
 } hrt_task_attr_t;
 
-/** Return the semantic version string, for example "0.4.0". */
+/** Return the semantic version string, for example "0.5.0". */
 const char *hrt_version_string(void);
 
 /** Return the version encoded as 0xMMmmpp. */
