30baec660a3bdcf7240098669ae251b5ab75fc6c
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-09-05 16:37:59 +0200

    test(hardrt): compile event and notification tests

--- cmake/hardrt_tester.cmake
+++ cmake/hardrt_tester.cmake
@@ -37,6 +37,8 @@ if(HARDRT_PORT STREQUAL "posix")
           ${CMAKE_SOURCE_DIR}/tests/test_external_tick.c
           ${CMAKE_SOURCE_DIR}/tests/test_idle_behavior.c
           ${CMAKE_SOURCE_DIR}/tests/test_mutex.c
+          ${CMAKE_SOURCE_DIR}/tests/test_event.c
+          ${CMAKE_SOURCE_DIR}/tests/test_notify.c
           ${CMAKE_SOURCE_DIR}/tests/test_now_ms.c
   )
 
